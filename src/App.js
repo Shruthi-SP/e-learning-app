59badc4de7d9c93b6908b47e92872033c9a04f29
Shruthi-SP <shrupr@gmail.com>
2022-02-16 09:15:37 +0530

    changing title with react helmet package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Container, Typography } from "@mui/material";
 import { withRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Helmet from "react-helmet";
 
 function App(props) {
   return (
     <div >
       <Container>
-        <Typography variant="h3" sx={{mt:3, mb:3 , textAlign:'center'}}>Welcome to e-learning app</Typography>
+        <Helmet>
+          <title>E-Learning App</title>
+          <meta name="description" content="E-Learning app" />
+        </Helmet>
+        <Typography variant="h3" sx={{ mt: 3, mb: 3, textAlign: 'center' }}>Welcome to e-learning app</Typography>
         <Navbar />
       </Container>
     </div>
